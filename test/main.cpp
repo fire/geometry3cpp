832988672c1656eeb8d92c2ee5e4951e76346e90
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2021-01-21 08:36:29 -0800

    Remove unused code.

--- test/main.cpp
+++ test/main.cpp
@@ -16,63 +16,6 @@
 using namespace g3;
 
 int main(int argc, char **argv) {
-  DMesh3 mesh;
-  mesh.AppendVertex(Vector3d(0, 0, 0));
-  mesh.AppendVertex(Vector3d(1, 0, 0));
-  mesh.AppendVertex(Vector3d(0, 1, 0));
-  mesh.AppendVertex(Vector3d(1, 1, 0));
-  int t0 = mesh.AppendTriangle(Vector3i(0, 1, 2));
-  int t1 = mesh.AppendTriangle(Vector3i(0, 3, 1));
-
-  bool bValid = mesh.CheckValidity();
-  mesh.CompactInPlace();
-  bValid = mesh.CheckValidity();
-
-  for (Vector3d v : mesh.Vertices()) {
-    std::cout << v[0] << " " << v[1] << " " << v[2] << std::endl;
-  }
-  for (Index3i t : mesh.Triangles()) {
-    std::cout << t[0] << " " << t[1] << " " << t[2] << std::endl;
-  }
-
-  for (int eid : mesh.BoundaryEdgeIndices()) {
-    Index4i ev = mesh.GetEdge(eid);
-    if (ev[3] != InvalidID)
-      std::cout << "BAD BOUNDARY EDGE ITR!!";
-  }
-
-  for (int vid : mesh.VertexIndices()) {
-    std::cout << "vertex " << vid << " tris: ";
-    for (int tid : mesh.VtxTrianglesItr(vid))
-      std::cout << tid << " ";
-    std::cout << std::endl;
-  }
-
-  DMeshAABBTree3 spatial(std::make_shared<DMesh3>(mesh), true);
-  spatial.Build();
-  spatial.TestCoverage();
-  double fNearDistSqr;
-
-  int n0 = 0, n1 = 0, fails = 0;
-  Wml::Mathd::IntervalRandom(-10, 10, 31337);
-  for (int k = 0; k < 1; ++k) {
-    double x = Wml::Mathd::IntervalRandom(-10, 10);
-    double y = Wml::Mathd::IntervalRandom(-10, 10);
-    double z = Wml::Mathd::IntervalRandom(-10, 10);
-    int near_tid = spatial.FindNearestTriangle(Vector3d(x, y, z), fNearDistSqr);
-    if (near_tid == 0)
-      n0++;
-    else
-      n1++;
-
-    int brute_near_tid =
-        MeshQueries::FindNearestTriangle_LinearSearch(mesh, Vector3d(x, y, z));
-    if (near_tid != brute_near_tid)
-      fails++;
-  }
-  std::cout << "near0 " << n0 << " near1 " << n1 << " fails " << fails
-            << std::endl;
-
   OBJReader reader;
   DMesh3Builder builder;
 
@@ -87,6 +30,7 @@ int main(int argc, char **argv) {
             << read_timer.ToString() << std::endl;
   auto mesh1 = builder.Meshes[0];
   std::cout << mesh1->MeshInfoString();
+  // TODO 2021-01-21 Color, UV, Groups aren't input // fire
 
   double cur_len =
       (mesh1->GetEdgePoint(0, 0) - mesh1->GetEdgePoint(0, 1)).norm();
@@ -94,14 +38,15 @@ int main(int argc, char **argv) {
   DMeshAABBTree3 spatialTest(mesh1, true);
   spatialTest.Build();
   spatialTest.TestCoverage();
-
   BlockTimer remesh_timer("remesh", true);
   Remesher r(mesh1);
   r.SetProjectionTarget(MeshProjectionTarget::AutoPtr(mesh1, true));
-  r.SmoothSpeedT = 1.0;
+  // http://www.gradientspace.com/tutorials/2018/7/5/remeshing-and-constraints
+  // TODO 2021-01-21 Fix all boundary edges // fire
+  // "half the edge length", approximately
+  r.SmoothSpeedT = 0.01;
   r.EnableParallelSmooth = true;
   r.PreventNormalFlips = true;
-  r.Precompute();
   for (int k = 0; k < 5; ++k) {
   	r.BasicRemeshPass();
 	std::cout << "remesh pass " << k << std::endl;
@@ -117,8 +62,4 @@ int main(int argc, char **argv) {
   write_meshes.push_back(WriteMesh(mesh1));
   OBJWriter writer;
   writer.Write(output, write_meshes, WriteOptions::Defaults());
-
-  Vector3d axis(0, 0, 1);
-  Matrix3d matrix;
-  g3::ComputeAlignZAxisMatrix(axis, matrix, false);
 }
